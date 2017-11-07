3cd4461acb4c1429df3ecf5786d34ad51cdf92e3
Mikolaj Leszczynski <leszczynski.mikolaj@gmail.com>
2017-11-07 14:50:16 +0000

    Nuked all non-essentials

--- nucleus-example-real-life/src/main/java/nucleus/example/util/Injector.java
+++
@@ -1,5 +0,0 @@
-package nucleus.example.util;
-
-public interface Injector {
-    void inject(Object target);
-}
--- nucleus-example-real-life/src/main/java/nucleus/example/util/adapters/ViewHolderType.java
+++
@@ -1,8 +0,0 @@
-package nucleus.example.util.adapters;
-
-import android.view.ViewGroup;
-
-public interface ViewHolderType<T> {
-    boolean isOfItem(Object item);
-    BaseViewHolder<T> create(ViewGroup parent);
-}
